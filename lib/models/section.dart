7bffdaff38b8c36b16e83376beea419453827ef2
JacoRochaDev <jacofernandes@outlook.com>
2022-11-04 14:30:22 -0300

    buscando as imagens da home do firebase

--- lib/models/section.dart
+++ lib/models/section.dart
@@ -1,11 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:loja_virtual/models/section_item.dart';
 
 class Section {
   Section.fromDocument(DocumentSnapshot document) {
     name = document['name'];
     type = document['type'];
+    items = (document['items'] as List<dynamic> ?? [])
+        .map((e) => SectionItem.fromMap(e as Map<String, dynamic>))
+        .toList();
   }
 
   String name;
   String type;
+  List<SectionItem> items;
+
+  @override
+  String toString() {
+    return 'Section{name: $name, type: $type, items: $items}';
+  }
 }
--- lib/models/section_item.dart
+++ lib/models/section_item.dart
@@ -1,1 +1,12 @@
-class SectionItem {}
+class SectionItem {
+  SectionItem.fromMap(Map<String, dynamic> map) {
+    image = map['image'];
+  }
+
+  String image;
+
+  @override
+  String toString() {
+    return 'SectionItem{image: $image}';
+  }
+}
