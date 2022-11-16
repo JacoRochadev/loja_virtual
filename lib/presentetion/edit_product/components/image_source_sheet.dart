92a9fb56c43f87701b02757b69f876f28e1fd70d
JacoRochaDev <jacofernandes@outlook.com>
2022-11-16 19:59:24 -0300

    exibindo item para ser utilizado no ios também

--- lib/presentetion/edit_product/components/image_source_sheet.dart
+++ lib/presentetion/edit_product/components/image_source_sheet.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ImageSourceSheet extends StatelessWidget {
@@ -5,40 +8,65 @@ class ImageSourceSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomSheet(
-      onClosing: () {},
-      builder: ((_) => Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  elevation: 0,
-                  primary: Colors.white,
+    if (Platform.isAndroid) {
+      return BottomSheet(
+        onClosing: () {},
+        builder: ((_) => Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    primary: Colors.white,
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Câmera',
+                    style: Theme.of(context).textTheme.subtitle1.copyWith(
+                          color: Colors.black,
+                        ),
+                  ),
                 ),
-                onPressed: () {},
-                child: Text(
-                  'Câmera',
-                  style: Theme.of(context).textTheme.subtitle1.copyWith(
-                        color: Colors.black,
-                      ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    primary: Colors.white,
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Galeria',
+                    style: Theme.of(context).textTheme.subtitle1.copyWith(
+                          color: Colors.black,
+                        ),
+                  ),
                 ),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  elevation: 0,
-                  primary: Colors.white,
-                ),
-                onPressed: () {},
-                child: Text(
-                  'Galeria',
-                  style: Theme.of(context).textTheme.subtitle1.copyWith(
-                        color: Colors.black,
-                      ),
-                ),
-              ),
-            ],
-          )),
-    );
+              ],
+            )),
+      );
+    } else {
+      return CupertinoActionSheet(
+        title: const Text('Selecionar foto para o item'),
+        message: const Text('Escolha a origem da foto'),
+        cancelButton: CupertinoActionSheetAction(
+          onPressed: Navigator.of(context).pop,
+          child: const Text('Cancelar'),
+        ),
+        actions: [
+          CupertinoActionSheetAction(
+            onPressed: () {},
+            child: const Text(
+              'Câmera',
+            ),
+          ),
+          CupertinoActionSheetAction(
+            onPressed: () {},
+            child: const Text(
+              'Galeria',
+            ),
+          ),
+        ],
+      );
+    }
   }
 }
