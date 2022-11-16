2834d4a8a8c34729ca8426cc4d69a3c7b64036bf
JacoRochaDev <jacofernandes@outlook.com>
2022-11-16 18:29:26 -0300

    ajustando o tamanho e cor das fontes

--- lib/presentetion/admin_users/admin_users_screen.dart
+++ lib/presentetion/admin_users/admin_users_screen.dart
@@ -21,19 +21,22 @@ class AdminUserScreen extends StatelessWidget {
               return ListTile(
                 title: Text(
                   adminUsersManager.users[index].name,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w800,
-                    color: Colors.white,
-                  ),
+                  style: Theme.of(context).textTheme.subtitle2.copyWith(
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white,
+                      ),
                 ),
                 subtitle: Text(
                   adminUsersManager.users[index].email,
-                  style: const TextStyle(
-                    color: Colors.white,
-                  ),
+                  style: Theme.of(context).textTheme.subtitle2.copyWith(
+                        color: Colors.white,
+                      ),
                 ),
               );
             },
+            highlightTextStyle: Theme.of(context).textTheme.headline6.copyWith(
+                  color: Colors.white,
+                ),
             indexedHeight: (index) => 80,
             strList: adminUsersManager.names,
             showPreview: true,
