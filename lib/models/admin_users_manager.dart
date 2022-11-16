4f34ef5db335bb9240c9bbdfd73e6db62eea265e
JacoRochaDev <jacofernandes@outlook.com>
2022-11-16 18:33:50 -0300

    buscando os usários do firebase

--- lib/models/admin_users_manager.dart
+++ lib/models/admin_users_manager.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:faker/faker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +7,7 @@ import 'package:loja_virtual/models/user_manager.dart';
 
 class AdminUserManager extends ChangeNotifier {
   List<UserModel> users = [];
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   void updateUser(UserManager userManager) {
     if (userManager.adminEnabled) {
@@ -14,16 +16,19 @@ class AdminUserManager extends ChangeNotifier {
   }
 
   void _listenToUsers() {
-    const faker = Faker();
+    //gerando dados falsos
+    // const faker = Faker();
 
-    for (int i = 0; i < 1000; i++) {
-      users.add(
-          UserModel(name: faker.person.name(), email: faker.internet.email()));
-    }
-    users
-        .sort(((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase())));
-
-    notifyListeners();
+    // for (int i = 0; i < 1000; i++) {
+    //   users.add(
+    //       UserModel(name: faker.person.name(), email: faker.internet.email()));
+    // }
+    firestore.collection('users').get().then((snapshot) {
+      users = snapshot.docs.map((e) => UserModel.fromDocument(e)).toList();
+      users.sort(
+          ((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase())));
+      notifyListeners();
+    });
   }
 
   List<String> get names => users.map((e) => e.name).toList();
