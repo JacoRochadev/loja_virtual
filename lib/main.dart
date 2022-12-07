ab56e48490a89d5b2d7a899ebc9a4c362be0cfe8
JacoRochaDev <jacofernandes@outlook.com>
2022-12-06 22:58:07 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:loja_virtual/models/home_manager.dart';
 import 'package:loja_virtual/presentetion/adress/adress_screen.dart';
 import 'package:loja_virtual/presentetion/base/base_screen.dart';
 import 'package:loja_virtual/presentetion/cart/cart_screen.dart';
-import 'package:loja_virtual/presentetion/checkout_screen/checkout_screen.dart';
+import 'package:loja_virtual/presentetion/checkout/checkout_screen.dart';
 import 'package:loja_virtual/presentetion/edit_product/edit_product_screen.dart';
 import 'package:loja_virtual/presentetion/login/login_screen.dart';
 import 'package:loja_virtual/presentetion/product/product_screen.dart';
