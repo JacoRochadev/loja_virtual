5bfc2b514e12a68fcbe8e441da926c7211279e25
JacoRochaDev <jacofernandes@outlook.com>
2023-03-02 15:06:48 -0300

    corrigindo bug

--- lib/presentetion/checkout/components/card_front.dart
+++ lib/presentetion/checkout/components/card_front.dart
@@ -35,83 +35,81 @@ class CardFront extends StatelessWidget {
         ),
         child: Padding(
           padding: const EdgeInsets.all(24.0),
-          child: Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                    child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: const [
-                        Icon(
-                          Icons.credit_card,
-                          color: Colors.white,
-                          size: 50,
-                        ),
-                      ],
-                    ),
-                    CardTextField(
-                      title: 'Número do Cartão',
-                      bold: true,
-                      hint: '0000.0000.0000.0000',
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [
-                        FilteringTextInputFormatter.digitsOnly,
-                        LengthLimitingTextInputFormatter(16),
-                      ],
-                      onFieldSubmitted: (_) {
-                        dateFocus.requestFocus();
-                      },
-                      focusNode: numberFocus,
-                      validator: (number) {
-                        if (number.isEmpty ||
-                            number.length != 16 ||
-                            detectCCType(number) == CreditCardType.unknown) {
-                          return 'Inválido';
-                        }
-                        return null;
-                      },
-                    ),
-                    CardTextField(
-                      title: 'Validade',
-                      bold: true,
-                      hint: '00/0000',
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [
-                        dateFormatter,
-                      ],
-                      focusNode: dateFocus,
-                      onFieldSubmitted: (_) {
-                        nameFocus.requestFocus();
-                      },
-                      validator: (date) {
-                        if (date.isEmpty || date.length != 7) {
-                          return 'Inválido';
-                        }
-                        return null;
-                      },
-                    ),
-                    CardTextField(
-                      title: 'Titular do Cartão',
-                      bold: true,
-                      hint: 'Jaco R Ferreira',
-                      keyboardType: TextInputType.text,
-                      onFieldSubmitted: (_) {
-                        finished();
-                      },
-                      focusNode: nameFocus,
-                      validator: (name) {
-                        if (name.isEmpty) {
-                          return 'Inválido';
-                        }
-                        return null;
-                      },
-                    ),
-                  ],
-                ))
-              ],
-            ),
+          child: Row(
+            children: [
+              Expanded(
+                  child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Icon(
+                        Icons.credit_card,
+                        color: Colors.white,
+                        size: 50,
+                      ),
+                    ],
+                  ),
+                  CardTextField(
+                    title: 'Número do Cartão',
+                    bold: true,
+                    hint: '0000.0000.0000.0000',
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                      LengthLimitingTextInputFormatter(16),
+                    ],
+                    onFieldSubmitted: (_) {
+                      dateFocus.requestFocus();
+                    },
+                    focusNode: numberFocus,
+                    validator: (number) {
+                      if (number.isEmpty ||
+                          number.length != 16 ||
+                          detectCCType(number) == CreditCardType.unknown) {
+                        return 'Inválido';
+                      }
+                      return null;
+                    },
+                  ),
+                  CardTextField(
+                    title: 'Validade',
+                    bold: true,
+                    hint: '00/0000',
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      dateFormatter,
+                    ],
+                    focusNode: dateFocus,
+                    onFieldSubmitted: (_) {
+                      nameFocus.requestFocus();
+                    },
+                    validator: (date) {
+                      if (date.isEmpty || date.length != 7) {
+                        return 'Inválido';
+                      }
+                      return null;
+                    },
+                  ),
+                  CardTextField(
+                    title: 'Titular do Cartão',
+                    bold: true,
+                    hint: 'Jaco R Ferreira',
+                    keyboardType: TextInputType.text,
+                    onFieldSubmitted: (_) {
+                      finished();
+                    },
+                    focusNode: nameFocus,
+                    validator: (name) {
+                      if (name.isEmpty) {
+                        return 'Inválido';
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ))
+            ],
           ),
         ));
   }
