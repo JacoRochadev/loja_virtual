ad79edbda9fcf9518a5a59a364602770dce94c9e
JacoRochaDev <jacofernandes@outlook.com>
2023-03-03 17:35:57 -0300

    .

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,33 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onCall((data, context) => {
-  return {data: "Hellow from Cloud Functions!!!"};
+admin.initializeApp(functions.config().firebase);
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+exports.helloWorld = functions.https.onCall((data, context) => {
+  return {data: "Hello from Firebase!"};
+});
+
+exports.getUserData = functions.https.onCall( async (data, context) => {
+  if (!context.auth) {
+    return {"data": "nenhum usuario logado"};
+  }
+  const snapshot = await admin.firestore().collection('users').doc(context.auth.uid).get();
+
+  return {"data": snapshot.data()};
+});
+
+exports.addMessage = functions.https.onCall( async (data, context) => {
+  console.log(data);
+
+    const snapshot = await admin.firestore().collection("messages").add(data);
+
+    return {"success": snapshot.id};
+});
+
+exports.onNewOrder = functions.firestore.document('orders/{orderId}').onCreate(async (snapshot, context) => {
+  const orderId = context.params.orderId;
+  console.log(orderId);
 });
--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
-  final response =
-      await FirebaseFunctions.instance.httpsCallable('helloWorld').call();
-  print(response.data);
 }
 
 class MyApp extends StatelessWidget {
